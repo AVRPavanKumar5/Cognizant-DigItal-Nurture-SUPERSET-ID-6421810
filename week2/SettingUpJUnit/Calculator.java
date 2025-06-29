27b7531ccea20080e243af35cb5835b928b6173a
AVRPavanKumar5 <avrpavank@gmail.com>
2025-06-29 17:56:18 +0530

    Delete week2 directory

--- week2/SettingUpJUnit/Calculator.java
+++
@@ -1,8 +0,0 @@
-package SettingUpJUnit;
-
-public class Calculator {
-    // Calculates the total invoice amount after applying tax
-    public double calculateInvoiceTotal(double subtotal, double taxRate) {
-        return subtotal + (subtotal * taxRate);
-    }
-}
