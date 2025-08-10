bfd668ecc3adce02bc0961b00dc8c7ab43eb3c94
AVRPavanKumar5 <avrpavank@gmail.com>
2025-08-10 22:37:04 +0530

    Delete 1 directory

--- 1/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-c:\Users\kumar\OneDrive\Documents\Pictures\Screenshots\Screenshot 2025-07-28 233145.pngc:\Users\kumar\OneDrive\Documents\Pictures\Screenshots\Screenshot 2025-07-28 233145.png
