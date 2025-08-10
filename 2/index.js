8c7dfe15cc79bc7807cea5945a051a0e79bcc050
AVRPavanKumar5 <avrpavank@gmail.com>
2025-08-10 22:36:39 +0530

    Add files via upload

---
+++ 2/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+// Or import CurrencyConvertor if rendering separately
+
+ReactDOM.render(<App />, document.getElementById('root'));
