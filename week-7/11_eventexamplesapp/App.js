01dce616b0fb1af9d96f9a434ea1e5e11f99c867
AVRPavanKumar5 <avrpavank@gmail.com>
2025-08-10 22:09:20 +0530

    Add files via upload

---
+++ week-7/11_eventexamplesapp/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Event handling examples</h1>
+      <p>React app for lab: 11_eventexamplesapp</p>
+    </div>
+  );
+}
+
+export default App;
