dcc997fac7e078da7b09438dc417e69e92d2bd3c
AVRPavanKumar5 <avrpavank@gmail.com>
2025-08-10 22:37:22 +0530

    Delete 2 directory

--- 2/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-// Or import CurrencyConvertor if rendering separately
-
-ReactDOM.render(<App />, document.getElementById('root'));
